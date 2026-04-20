abd4e9c325a5dcb6ebd84978f408313a49a52a0f
2024125079 <167060056+2024125079@users.noreply.github.com>
2026-04-20 16:48:28 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,1 +1,28 @@
-1
+#include <stdio.h>
+int main(void){
+    int answer = 0;
+    int input = 0;
+    int count = 0;
+    int i = 0;
+    scanf("%d",&answer);
+
+    do{
+        scanf("%d",&input);
+        if(input > answer){
+            printf("%d>?\n",input); 
+            count ++;
+        }
+        else if(input < answer) {
+            printf("%d < ?\n", input); 
+            count ++;
+        }
+        else if(input == answer){
+        printf("%d ==?\n",input); 
+        ++count;
+        printf("%d\n",input, count);
+        }
+    }while(input != answer);
+
+    
+    return 0;
+}
