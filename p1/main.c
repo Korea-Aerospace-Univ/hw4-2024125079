d4f00e267c374b2bdf9cf5378f505499514b5422
2024125079 <167060056+2024125079@users.noreply.github.com>
2026-04-20 16:41:00 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -1,28 +1,2 @@
-#include <stdio.h>
-int main(void){
-    int answer = 0;
-    int input = 0;
-    int count = 0;
-    int i = 0;
-    scanf("%d",&answer);
-
-    do{
-        scanf("%d",&input);
-        if(input > answer){
-            printf("%d>?\n",input); 
-            count ++;
-        }
-        else if(input < answer) {
-            printf("%d < ?\n", input); 
-            count ++;
-        }
-        else if(input == answer){
-        printf("%d ==?\n",input); 
-        ++count;
-        printf("%d 시도횟수는 %d회\n",input, count);
-        }
-    }while(input != answer);
-
+1
     
-    return 0;
-}
