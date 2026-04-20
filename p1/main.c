838f9e66f536ee48a279d28a142152963a556dc9
2024125079 <167060056+2024125079@users.noreply.github.com>
2026-04-20 20:48:59 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -11,13 +11,13 @@ int main(void){
         // 예측한 값이 정답될 때까지 반복되도록 do while 반복문 시작에서 scanf로 geuss변수로 입력 받음
         count ++;       // 예측값 입력 후에 입력한 횟수 +1
         if(guess > answer){     // 예측한 값이 answer보다 클 때 예측한 값이 정답보다 크다고 출력 
-            printf("%d>?\n",guess);
+            printf("%d > ?\n",guess);
         }
         else if(guess < answer) {       // 예측한 값이 answer보다 작을 때 에측한 값이 정답보다 작다고 출력
             printf("%d < ?\n", guess); 
         }
         else if(guess == answer){       // 예측한 값이랑 정답이 같을때 정답이라고 출력
-        printf("%d ==?\n",guess); 
+        printf("%d == ?\n",guess); 
         printf("%d\n", count);        // 지금까지 센 횟수 출력
         }
     }while(guess != answer);        
